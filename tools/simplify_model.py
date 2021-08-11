05b7499775a367c7c9ca226b6c1e1ab4faf19999
qinhj <qinhj@lsec.cc.ac.cn>
2021-08-11 22:58:39 +0800

    [tools] review and update py script for simplify model

--- tools/simplify_model.py
+++ tools/simplify_model.py
@@ -6,32 +6,13 @@ from onnx import numpy_helper, helper
 import numpy as np
 from copy import deepcopy
 
-## replace op MatMul by Conv
-def matmul_to_conv2d(node, init_dict):
-    node.op_type = "Conv"
-    weight_name = node.input[1]
-    weight_tensor = init_dict[weight_name]
-    weight = numpy_helper.to_array(weight_tensor)
-    weight = np.expand_dims(weight.transpose(1,0), [2,3])
-    weight_tensor = numpy_helper.from_array(weight, name=weight_name)
-    init_dict[weight_name] = weight_tensor
 
-
-## replace op ReduceMax by MaxPool
-def reducemax_to_maxpool(node, model):
-    node = helper.make_node(op_type="MaxPool", inputs=node.input, \
-                            outputs=node.output, name=node.name,  \
-                            ceil_mode = 0, kernel_shape = [1,20], \
-                            pads = [0,0,0,0], strides=[1,1])
-    model.graph.node.append(node)
-
-
-## update input tensor("repeats") of Tile
-def convert_tile(node, init_dict):
-    arr_name = node.input[1]
-    arr = np.array([1,1,1,20], np.int64)
-    tensor = numpy_helper.from_array(arr, name=arr_name)
-    init_dict[arr_name] = tensor
+## map: name -> index
+def map_name_to_index(graph_prop_list):
+    m = {}
+    for i, n in enumerate(graph_prop_list):
+        m[n.name] = i
+    return m
 
 
 ## transpose input data from nhwc to nchw
@@ -70,15 +51,15 @@ def simplify_model(model_path):
     return simplify(model)
 
 
-def simplify_pfe_rpn_model(pfe_model_path, rpn_model_path):
+def simplify_pfn_rpn_model(pfn_model_path, rpn_model_path):
     """
-    @param: [in/out]pfe_model_path
+    @param: [in/out]pfn_model_path
     @param: [in/out]rpn_model_path
     """
-    model, check = simplify_model(pfe_model_path)
+    model, check = simplify_model(pfn_model_path)
     if not check:
-        print("[ERROR]: Simplify %s error!" % pfe_model_path)    
-    onnx.save(model, pfe_model_path)
+        print("[ERROR]: Simplify %s error!" % pfn_model_path)
+    onnx.save(model, pfn_model_path)
 
     model, check = simplify_model(rpn_model_path)
     if not check:
@@ -88,20 +69,17 @@ def simplify_pfe_rpn_model(pfe_model_path, rpn_model_path):
 
 if __name__ == "__main__":
 
-    pfe_model_path = "./onnx_model/pfe.onnx"
+    pfn_model_path = "./onnx_model/pfn.onnx"
     rpn_model_path = "./onnx_model/rpn.onnx"
 
-    # simplify pfe && rpn model inplace
-    simplify_pfe_rpn_model(pfe_model_path, rpn_model_path)
+    # simplify pfn && rpn model inplace
+    simplify_pfn_rpn_model(pfn_model_path, rpn_model_path)
     
-    # load pfe model
-    model = onnx.load(pfe_model_path)
+    ## ---------------- optimize pfn model ---------------- ##
+    
+    # load pfn model
+    model = onnx.load(pfn_model_path)
 
-    # cache all model initializer as a map(name -> initializer)
-    init_dict = {}
-    for init_node in model.graph.initializer:
-        init_dict[init_node.name] = init_node
-    
     # delete nodes
     delete_dict = {}
     for node in model.graph.node:
@@ -113,32 +91,48 @@ if __name__ == "__main__":
     for idx in range(val_len):
         model.graph.value_info.pop()
 
-    # pop all model initializer
-    init_len = len(model.graph.initializer)
-    for idx in range(init_len):
-        model.graph.initializer.pop()
-
     convert_input_nhwc_nchw(model)
 
-    rm_list = []
-    # modify some Ops
-    for node in model.graph.node:
-        # convert MatMul to Conv2D
+    # map: name -> index
+    map_init = map_name_to_index(model.graph.initializer)
+
+    # modify some op nodes
+    for i, node in enumerate(model.graph.node):
+        # update MatMul as Conv2D
         if node.op_type == "MatMul":
-            matmul_to_conv2d(node, init_dict)
+            node.op_type = "Conv"
+            # tensor -> array -> expand_dims -> tensor
+            weight_name = node.input[1]
+            weight_index = map_init[weight_name]
+            weight_tensor = model.graph.initializer[weight_index]
+            model.graph.initializer.remove(weight_tensor)
+            weight_array = numpy_helper.to_array(weight_tensor).transpose(1,0)
+            weight_array = np.expand_dims(weight_array, axis=-1)
+            weight_array = np.expand_dims(weight_array, axis=-1)
+            weight_tensor = numpy_helper.from_array(weight_array, name=weight_name)
+            model.graph.initializer.insert(weight_index, weight_tensor)
         
         # skip delete nodes by resetting the input tensor
         if node.input[0] in delete_dict.keys():
             node.input[0] = delete_dict[node.input[0]].input[0]
         
-        # convert ReduceMax to MaxPool
+        # replace ReduceMax by MaxPool
         if node.op_type == "ReduceMax":
-            rm_list.append(node)
-            reducemax_to_maxpool(node, model)
+            model.graph.node.remove(node)
+            node = helper.make_node(op_type="MaxPool",
+                inputs=node.input, outputs=node.output, name=node.name,
+                ceil_mode = 0, kernel_shape = [1,20], pads = [0,0,0,0], strides=[1,1])
+            model.graph.node.insert(i, node)
         
         # update input tensor("repeats") of Tile
         if node.op_type == "Tile":
-            convert_tile(node, init_dict)
+            arr_name = node.input[1]
+            arr_index = map_init[arr_name]
+            arr_tensor = model.graph.initializer[arr_index]
+            model.graph.initializer.remove(arr_tensor)
+            arr_array = np.array([1,1,1,20], np.int64)
+            arr_tensor = numpy_helper.from_array(arr_array, name=arr_name)
+            model.graph.initializer.insert(arr_index, arr_tensor)
         
         # update "axis" to 1 of Concat
         if node.op_type == "Concat":
@@ -149,16 +143,16 @@ if __name__ == "__main__":
         if node.name in delete_dict.keys():
             node.name = delete_dict[node.name].input[0]
 
-    # reset all model initializer
-    for name, tensor in init_dict.items():
-        model.graph.initializer.append(tensor)
     # remove target nodes
-    for keys,node in delete_dict.items():
-        model.graph.node.remove(node)
-    # remove unsupported(?) nodes: ReduceMax
-    for node in rm_list:
+    for keys, node in delete_dict.items():
         model.graph.node.remove(node)
 
-    pfe_model_save_path = "./onnx_model/pfe.opt.onnx"
-    onnx.save(model, pfe_model_save_path)
+    # do infer onnx model's shapes
+    model = onnx.shape_inference.infer_shapes(model)
+    # check and simplify onnx model
+    import onnxsim
+    model, _ = onnxsim.simplify(model)
+
+    pfn_model_save_path = "./onnx_model/pfn.opt.onnx"
+    onnx.save(model, pfn_model_save_path)
     print("Done")
