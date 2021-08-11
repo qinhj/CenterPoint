d0fc0ca6506e1d1ef91989515c8ef3b7ac9701e9
qinhj <qinhj@lsec.cc.ac.cn>
2021-08-11 16:31:19 +0800

    [tools] code review && refactor

--- tools/export_pointpillars_onnx.py
+++ tools/export_pointpillars_onnx.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+'''
 import argparse
 import copy
 import json
@@ -8,14 +12,16 @@ try:
     import apex
 except:
     print("No APEX!")
-import numpy as np
+'''
+#import numpy as np
 import torch
 from torch import nn
-import yaml
-from det3d import torchie
+#import yaml
+#from det3d import torchie
 from det3d.datasets import build_dataloader, build_dataset
 from det3d.models import build_detector
 from det3d.torchie import Config
+'''
 from det3d.torchie.apis import (
     batch_processor,
     build_optimizer,
@@ -25,21 +31,23 @@ from det3d.torchie.apis import (
     train_detector,
     example_to_device,
 )
+'''
+from det3d.torchie.apis import example_to_device
 from det3d.torchie.trainer import load_checkpoint
-import pickle 
-import time 
-from matplotlib import pyplot as plt 
+#import pickle
+#import time
+#from matplotlib import pyplot as plt
 from det3d.torchie.parallel import collate, collate_kitti
 from torch.utils.data import DataLoader
-import matplotlib.cm as cm
-import subprocess
-import cv2
-from tools.demo_utils import visual 
-from collections import defaultdict
+#import matplotlib.cm as cm
+#import subprocess
+#import cv2
+#from tools.demo_utils import visual
+#from collections import defaultdict
 
 
 class PointPillars(nn.Module):
-    def __init__(self,model):
+    def __init__(self, model):
         super(PointPillars, self).__init__()
         self.model = model
     
@@ -51,18 +59,17 @@ class PointPillars(nn.Module):
             preds[task]['dim'] = torch.exp(preds[task]['dim'])
             scores, labels = torch.max(hm_preds, dim=1)
             preds[task]["hm"] = (scores, labels)
-
         return preds
 
 
-
 def main():
-    cfg = Config.fromfile('configs/nusc/pp/nusc_centerpoint_pp_02voxel_two_pfn_10sweep_demo_export_onnx.py')
+    cfg_file = 'configs/nusc/pp/nusc_centerpoint_pp_02voxel_two_pfn_10sweep_demo_export_onnx.py'
+    cfg = Config.fromfile(cfg_file)
     
     model = build_detector(cfg.model, train_cfg=None, test_cfg=cfg.test_cfg)
 
+    # create dataset loader
     dataset = build_dataset(cfg.data.val)
-
     data_loader = DataLoader(
         dataset,
         batch_size=1,
@@ -73,38 +80,53 @@ def main():
         pin_memory=False,
     )
 
+    # load checkpoint(gpu) to cpu
     checkpoint = load_checkpoint(model, './latest.pth', map_location="cpu")
+    # for what?
     model.eval()
-    
+
+    # model to gpu
     model = model.cuda()
 
     gpu_device = torch.device("cuda")
 
-    points_list = [] 
-    gt_annos = [] 
-    detections  = [] 
+    #gt_annos = []
+    #detections = []
     
     data_iter = iter(data_loader)
     data_batch = next(data_iter)
-    
+
+    # create rpn model
     pp_model = PointPillars(model)
 
-    points = data_batch['points'][:, 1:4].cpu().numpy()
-    with torch.no_grad():
+    #points_list = []
+    #points = data_batch['points'][:, 1:4].cpu().numpy()
 
+    with torch.no_grad():
+        # convert tensor data to gpu
         example = example_to_device(data_batch, gpu_device, non_blocking=False)
-        example["voxels"] = torch.zeros((example["voxels"].shape[0],example["voxels"].shape[1],10),dtype=torch.float32,device=gpu_device)
-        
-        
+
+        # reset tensor data(maybe from "voxel" to "pp")
+        example["voxels"] = torch.zeros(
+            (example["voxels"].shape[0], example["voxels"].shape[1], 10),
+            dtype=torch.float32, device=gpu_device)
+
+        # pop useless items
         example.pop("metadata")
         example.pop("points")
-
+        # reset tensor shape?
         example["shape"] = torch.tensor(example["shape"], dtype=torch.int32, device=gpu_device)
+
+        # forward once
         model(example)
-        torch.onnx.export(model.reader, (example["voxels"],example["num_voxels"],example["coordinates"]),"onnx_model/pfe.onnx",opset_version=11)
-        
-        rpn_input  = torch.zeros((1,64,512,512),dtype=torch.float32,device=gpu_device)
-        torch.onnx.export(pp_model, rpn_input,"onnx_model/rpn.onnx",opset_version=11)
+        # export pfe model to onnx
+        pfe_input = (example["voxels"], example["num_voxels"], example["coordinates"])
+        torch.onnx.export(model.reader, pfe_input ,"onnx_model/pfe.onnx", opset_version=11)
+
+        # export rpn model to onnx
+        rpn_input = torch.zeros((1,64,512,512), dtype=torch.float32, device=gpu_device)
+        torch.onnx.export(pp_model, rpn_input, "onnx_model/rpn.onnx", opset_version=11)
+
     print("Done")
 
 
--- tools/simplify_model.py
+++ tools/simplify_model.py
@@ -5,43 +5,19 @@ import onnx
 from onnx import numpy_helper, helper
 import numpy as np
 from copy import deepcopy
-from onnxsim import simplify
 
-
+## replace op MatMul by Conv
 def matmul_to_conv2d(node, init_dict):
+    node.op_type = "Conv"
     weight_name = node.input[1]
     weight_tensor = init_dict[weight_name]
     weight = numpy_helper.to_array(weight_tensor)
-    weight = np.expand_dims(weight.transpose(1,0),[2,3])
+    weight = np.expand_dims(weight.transpose(1,0), [2,3])
     weight_tensor = numpy_helper.from_array(weight, name=weight_name)
     init_dict[weight_name] = weight_tensor
 
-def delete_init(model):
-    init_len = len(model.graph.initializer)
-    for i in range(init_len):
-        model.graph.initializer.pop()
 
-def convert_input_nhwc_nchw(model):
-    batch_dim = 1
-    dim_list = [dim_val.dim_value for dim_val in model.graph.input[0].type.tensor_type.shape.dim]
-    dim_list.insert(0, batch_dim)
-    dim_list = np.array(dim_list)[[0,3,1,2]]
-   
-    input_node = onnx.helper.make_tensor_value_info('input.1', \
-                                                    onnx.TensorProto.FLOAT, dim_list.tolist())
-    model.graph.input.pop()
-    model.graph.input.append(input_node)
-    
-    dim_list = [dim_val.dim_value for dim_val in model.graph.output[0].type.tensor_type.shape.dim]
-    dim_list.insert(0, batch_dim)
-    dim_list.insert(2, 1)
-    dim_list = np.array(dim_list)[[0,3,1,2]]
-   
-    out_node = onnx.helper.make_tensor_value_info(model.graph.output[0].name, \
-                                                  onnx.TensorProto.FLOAT, dim_list.tolist())
-    model.graph.output.pop()
-    model.graph.output.append(out_node)
-    
+## replace op ReduceMax by MaxPool
 def reducemax_to_maxpool(node, model):
     node = helper.make_node(op_type="MaxPool", inputs=node.input, \
                             outputs=node.output, name=node.name,  \
@@ -49,92 +25,140 @@ def reducemax_to_maxpool(node, model):
                             pads = [0,0,0,0], strides=[1,1])
     model.graph.node.append(node)
 
+
+## update input tensor("repeats") of Tile
 def convert_tile(node, init_dict):
     arr_name = node.input[1]
-    arr = np.array([1,1,1,20],np.int64)
+    arr = np.array([1,1,1,20], np.int64)
     tensor = numpy_helper.from_array(arr, name=arr_name)
     init_dict[arr_name] = tensor
+
+
+## transpose input data from nhwc to nchw
+def convert_input_nhwc_nchw(model):
+    batch_dim = 1
+
+    # input tensor dim
+    dim_list = [dim_val.dim_value for dim_val in model.graph.input[0].type.tensor_type.shape.dim]
+    dim_list.insert(0, batch_dim) # as nhwc
+    dim_list = np.array(dim_list)[[0,3,1,2]] # -> nchw
+
+    # reset input tensor value info
+    input_node = onnx.helper.make_tensor_value_info(
+        'input.1', onnx.TensorProto.FLOAT, dim_list.tolist())
+    model.graph.input.pop()
+    model.graph.input.append(input_node)
+
+    # output tensor dim
+    dim_list = [dim_val.dim_value for dim_val in model.graph.output[0].type.tensor_type.shape.dim]
+    dim_list.insert(0, batch_dim) # as nhwc
+    dim_list.insert(2, 1)
+    dim_list = np.array(dim_list)[[0,3,1,2]] # -> nchw
+
+    # reset output tensor value info
+    out_node = onnx.helper.make_tensor_value_info(
+        model.graph.output[0].name, onnx.TensorProto.FLOAT, dim_list.tolist())
+    model.graph.output.pop()
+    model.graph.output.append(out_node)
+
 
 def simplify_model(model_path):
     model = onnx.load(model_path)
     if model is None:
-        print("File %s is not find! "%model_path)
+        print("File %s not found!" % model_path)
+    from onnxsim import simplify
     return simplify(model)
 
+
 def simplify_pfe_rpn_model(pfe_model_path, rpn_model_path):
-    
+    """
+    @param: [in/out]pfe_model_path
+    @param: [in/out]rpn_model_path
+    """
     model, check = simplify_model(pfe_model_path)
     if not check:
-        print("[ERROR]:Simplify %s error!"% pfe_model_path)    
+        print("[ERROR]: Simplify %s error!" % pfe_model_path)    
     onnx.save(model, pfe_model_path)
 
     model, check = simplify_model(rpn_model_path)
     if not check:
-        print("[ERROR]:Simplify %s error!"% rpn_model_path)    
+        print("[ERROR]: Simplify %s error!" % rpn_model_path)    
     onnx.save(model, rpn_model_path)
-
 
 
 if __name__ == "__main__":
 
     pfe_model_path = "./onnx_model/pfe.onnx"
-    pfe_model_save_path = "./onnx_model/pfe_sim.onnx"
-
     rpn_model_path = "./onnx_model/rpn.onnx"
-  
+
+    # simplify pfe && rpn model inplace
     simplify_pfe_rpn_model(pfe_model_path, rpn_model_path)
     
-    # modify pfe model
+    # load pfe model
     model = onnx.load(pfe_model_path)
+
+    # cache all model initializer as a map(name -> initializer)
     init_dict = {}
     for init_node in model.graph.initializer:
         init_dict[init_node.name] = init_node
     
-    #delete nodes
+    # delete nodes
     delete_dict = {}
     for node in model.graph.node:
         if node.op_type in {"Transpose", "Expand", "Squeeze"}:
             delete_dict[node.output[0]] = node
 
+    # pop all model value info
     val_len = len(model.graph.value_info)
     for idx in range(val_len):
         model.graph.value_info.pop()
 
-    delete_init(model)
+    # pop all model initializer
+    init_len = len(model.graph.initializer)
+    for idx in range(init_len):
+        model.graph.initializer.pop()
 
-    matmul_weight_name = []
     convert_input_nhwc_nchw(model)
+
     rm_list = []
+    # modify some Ops
     for node in model.graph.node:
-        
         # convert MatMul to Conv2D
         if node.op_type == "MatMul":
-            node.op_type = "Conv"
             matmul_to_conv2d(node, init_dict)
         
+        # skip delete nodes by resetting the input tensor
         if node.input[0] in delete_dict.keys():
             node.input[0] = delete_dict[node.input[0]].input[0]
         
-        # convert ReduceMax to maxpool
+        # convert ReduceMax to MaxPool
         if node.op_type == "ReduceMax":
             rm_list.append(node)
             reducemax_to_maxpool(node, model)
+        
+        # update input tensor("repeats") of Tile
         if node.op_type == "Tile":
             convert_tile(node, init_dict)
+        
+        # update "axis" to 1 of Concat
         if node.op_type == "Concat":
             node.attribute[0].i = 1
+
+    # update/check output tensor value info
     for node in model.graph.output:
         if node.name in delete_dict.keys():
             node.name = delete_dict[node.name].input[0]
 
-    for name,tensor in init_dict.items():
+    # reset all model initializer
+    for name, tensor in init_dict.items():
         model.graph.initializer.append(tensor)
-        
+    # remove target nodes
     for keys,node in delete_dict.items():
         model.graph.node.remove(node)
-
+    # remove unsupported(?) nodes: ReduceMax
     for node in rm_list:
         model.graph.node.remove(node)
 
+    pfe_model_save_path = "./onnx_model/pfe.opt.onnx"
     onnx.save(model, pfe_model_save_path)
     print("Done")
